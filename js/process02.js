f51dc2b9d5f0dc0b1788fa89c670a63d7849ca96
renatacamp <rcampuzanoc@centro.edu.mx>
2020-04-22 10:49:03 -0500

    loader index

--- js/process02.js
+++ js/process02.js
@@ -3,22 +3,15 @@ window.addEventListener("load", () => {
     let page = path.split("/").pop();
     let n = page.split(".")[0];
 
-    let looks = document.getElementsByClassName("looks");
-    for (let i = 0; i < looks.length; i++) {
-        looks[i].src = "./assets/" + n + "/" + (i + 1) + ".jpg";
-    }
 
-    let anim = document.getElementById("video_coleccion1");
-    anim.src = "./assets/designers/" + n + ".mp4";
+    let anim = document.getElementById("videoInteractivo1");
+    anim.src = "./assets/opening2.mp4";
 
+    let v2 = document.getElementById("videoInteractivo");
+    v2.src = "./assets/clip2_2.mp4";
 
-    let rand = Math.round(Math.random() * 13) + 1;
-    let v2 = document.getElementById("video2");
-    v2.src = "./assets/interiors/interior" + rand + ".mp4";
-
-    let v3 = document.getElementById("video3");
-    rand = Math.round(Math.random() * 13) + 1;
-    v3.src = "./assets/interiors/interior" + rand + ".mp4";
+    let v3 = document.getElementById("videoInteractivo2");
+    v3.src = "./assets/clip_movil.mp4";
 
     const loader = document.getElementById("loader-clean");
     loader.className += " hidden";
